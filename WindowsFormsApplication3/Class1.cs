1afb70e4211ebb5774fa8785cd9ff1d3c6cf7721
toto <devildarck@hotmail.com>
2014-10-08 00:42:27 -0400

    prueba int o

--- WindowsFormsApplication3/Class1.cs
+++ WindowsFormsApplication3/Class1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +9,6 @@ namespace WindowsFormsApplication3
     class Class1
     {
     int a ;
+    int o = 0;
     }
 }
